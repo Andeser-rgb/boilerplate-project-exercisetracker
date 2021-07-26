8e2b97dd0626180127e9a60c889907b07a2c4267
Andeser-rgb <andeser@protonmail.com>
2021-07-26 09:30:32 +0200

    Added function to creat new users

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser');
 require('dotenv').config()
+const mongoose = require('mongoose');
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+app.use(bodyParser.urlencoded({extended: false}));
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+const personSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date
+});
 
+const Person = new mongoose.model("Person", personSchema);
+
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  Person.find({username: username}, (err, data) => {
+    if(data.length <= 0){
+      const newPerson = new Person({username: req.body.username});
+      newPerson.save((err, data) => {console.log(err)});
+      res.send(newPerson);
+    }
+    else {
+      res.send({username: data[0].username, _id: data[0]._id});
+    }
+
+  });
+
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
